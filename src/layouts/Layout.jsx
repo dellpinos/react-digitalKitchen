fa03368e594305ba6ed32e1c01dfdf91252c7d5c
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2023-12-19 21:44:02 -0300

    commit inicial

---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,7 @@
+import { Outlet } from 'react-router-dom';
+export default function Layout() {
+  return (
+    <div>Laburanding con React < Outlet/> </div>
+
+  )
+}
---
+++ src/views/Registro.jsx
@@ -0,0 +1,25 @@
+export default function Registro() {
+    return (
+        <>
+
+            <h1 className=" text-4xl font-black">Crear Cuenta</h1>
+            <p>Llena el formulario!</p>
+
+            <div className=" bg-white shadow-md rounded-md mt-10 px-5 py-10">
+                <form >
+                    <div className=" mb-4">
+                        <label>
+
+                        </label>
+                        <input
+                            type="text"
+                            
+                        />
+
+
+                    </div>
+                </form>
+            </div>
+        </>
+    )
+}
